f31910affc80d8af5a33da8153b9f175684fe113
POOJA <poojap0703@gmail.com>
2025-02-14 11:32:34 +0530

    14 Feb Commit

--- src/ex_10_Functions/Lab_62_functions.java
+++ src/ex_10_Functions/Lab_62_functions.java
@@ -17,6 +17,15 @@ public class Lab_62_functions {
 
         int a = age_to_vote();
         System.out.println("Age to vote is: " +a);
+
+        greet_with_name("Pooja");
+
+        greet_with_full_name("Pooja ", "Prajapati");
+
+        details("Pooja", 24, 4000000);
+
+        int sum = sum_of_a_b(10, 20);
+        System.out.println("Sum of a and b is:" +sum);
     }
 
     //1. Without Parameters and Without Return Type.
@@ -38,4 +47,28 @@ public class Lab_62_functions {
     }
 
     //3. With Parameters and Without Return Type
+    static void greet_with_name(String name)
+    {
+        System.out.println("Hi " +name );
+    }
+
+    static void greet_with_full_name(String f_name, String l_name)
+    {
+        System.out.println("Hi i am " +f_name + l_name  );
+    }
+
+    static void details(String name, int age, double salary)
+    {
+        System.out.println("Hello my name is: "+name+ "\nmy age is: "+age+ "\nmy salary is: " +salary);
+    }
+
+    //4. With Parameters and With Return Type
+
+    static int sum_of_a_b(int a, int b)
+    {
+        return a+b;
+    }
+
 }
+
+//52:50
